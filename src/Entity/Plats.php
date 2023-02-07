a3d99f8ef2ed491ff149aa945181c7566122bc0d
sylvain <sylvain@delmote.me>
2023-02-07 09:40:21 +0100

    first Commit

---
+++ src/Entity/Plats.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\PlatsRepository;
+use Doctrine\ORM\Mapping as ORM;
+use phpDocumentor\Reflection\PseudoTypes\False_;
+
+#[ORM\Entity(repositoryClass: PlatsRepository::class)]
+#[ApiResource]
+class Plats
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $image = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $recette = null;
+
+    #[ORM\Column]
+    private ?bool $healthy = False;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getRecette(): ?string
+    {
+        return $this->recette;
+    }
+
+    public function setRecette(?string $recette): self
+    {
+        $this->recette = $recette;
+
+        return $this;
+    }
+
+    public function isHealthy(): ?bool
+    {
+        return $this->healthy;
+    }
+
+    public function setHealthy(bool $healthy): self
+    {
+        $this->healthy = $healthy;
+
+        return $this;
+    }
+}
